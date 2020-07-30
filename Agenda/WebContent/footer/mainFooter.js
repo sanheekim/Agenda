086243148840b5f20e7dd7b656bf07f4dc8989de
jayoung-cinnamon <coffeejayoung@gmail.com>
2020-07-30 11:28:09 +0900

    육자영 : main footer 수정 중

---
+++ Agenda/WebContent/footer/mainFooter.js
@@ -0,0 +1,8 @@
+/** @format */
+
+let mouseCousor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", function (e) {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+});
