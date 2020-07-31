f7bf6dee6907c2028d86a755cd19ad82f14e3e73
jayoung-cinnamon <coffeejayoung@gmail.com>
2020-07-31 13:27:28 +0900

    육자영 : /loginController(logout기능추가)

--- Agenda/src/com/agenda/login/LoginController.java
+++ Agenda/src/com/agenda/login/LoginController.java
@@ -10,14 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-
 @WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public LoginController() {
-				System.out.println("컨트롤러로 넘어옴");
+		System.out.println("컨트롤러로 넘어옴");
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -26,50 +24,43 @@ public class LoginController extends HttpServlet {
 		doPost(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
-		
-		String command = request.getParameter("command");		
+
+		String command = request.getParameter("command");
 		System.out.println(command);
-		
-		//로그인페이지
-		if(command.equals("login")) {
+		HttpSession session = request.getSession();
+		// 로그인페이지
+		if (command.equals("login")) {
+
 			
-			HttpSession session = request.getSession();
 			String member_id = request.getParameter("member_id");
 			String member_pw = request.getParameter("member_pw");
 
 			LoginDao dao = new LoginDao();
 //			dao.login(member_id, member_pw);
-			
-			LoginDto input  = new LoginDto(member_id,member_pw);
+
+			LoginDto input = new LoginDto(member_id, member_pw);
 			LoginDto dto = dao.login(input);
-			System.out.println("dto="+dto);
-			
-			
+			System.out.println("dto=" + dto);
+
 			System.out.println(member_id);
 			System.out.println(member_pw);
-	
-			
-			
-			
-			
-			
-			
-			if(dto != null) {
-				
-				session.setAttribute("dto",dto);
-				session.setMaxInactiveInterval(10*60);
-				
+
+			if (dto != null) {
+
+				session.setAttribute("dto", dto);
+				session.setMaxInactiveInterval(10 * 60);
+
 				System.out.println("DTO 있음  = mypage main으로 보냄");
 				// User, Admin 구분하지 않고 바로 main으로 보내기
 				RequestDispatcher dispatch = request.getRequestDispatcher("header/loginMain.jsp");
 				dispatch.forward(request, response);
-				//response.sendRedirect("header/loginMain.jsp");
-				
-				
+				// response.sendRedirect("header/loginMain.jsp");
+
 //			if(dto.getMember_role().equals("ADMIN")) {				
 //				
 //		    RequestDispatcher dispatch = request.getRequestDispatcher("login/loginAdmin.jsp");
@@ -85,33 +76,37 @@ public class LoginController extends HttpServlet {
 //				dispatch.forward(request,response);			
 //				response.sendRedirect(".login/loginUser.jsp");
 //			}
-				
-				
+
 			} else {
 				System.out.println("dto is null : login form 으로 다시 보냄");
-				loginResponse("아이디 또는 비밀번호가 틀렸습니다","loginForm.jsp",response);
-				//response.sendRedirect("login/loginForm.jsp"); 
-			
+				loginResponse("아이디 또는 비밀번호가 틀렸습니다", "login/loginForm.jsp", response);
+				// response.sendRedirect("login/loginForm.jsp");
+
 			}
-			 
+
+		}
+
+		else if (command.equals("logout")) {
+
+			// session 객체가 가진 값 삭제
+			session.invalidate();
+			response.sendRedirect("main/main.jsp");
 
 		}
 
 	}
 
-	
 	public void loginResponse(String msg, String url, HttpServletResponse response) throws IOException {
-		String js = "<script type = 'text/javascript'>"
-					+"alert('"+msg+"');"
-					+"location.href='"+url+"';"
-					+"</script>";
-		
+		String js = "<script type = 'text/javascript'>" + "alert('" + msg + "');" + "location.href='" + url + "';"
+				+ "</script>";
+
 		response.getWriter().append(js);
 	}
-	
-	 private void dispatch(HttpServletRequest request, HttpServletResponse
-	  response, String url) throws ServletException, IOException {
-	 RequestDispatcher dispatch = request.getRequestDispatcher(url);
-	 dispatch.forward(request, response); }
-	 
+
+	private void dispatch(HttpServletRequest request, HttpServletResponse response, String url)
+			throws ServletException, IOException {
+		RequestDispatcher dispatch = request.getRequestDispatcher(url);
+		dispatch.forward(request, response);
+	}
+
 }
