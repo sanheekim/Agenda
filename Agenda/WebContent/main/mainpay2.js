f3cfbd0a391fdd8268a2b7fcdcc40c59a39776fa
sanheekim <68312957+sanheekim@users.noreply.github.com>
2020-08-04 18:09:58 +0900

    산희 - main.jsp, mainpay1~4.js 수정&추가 - 메인페이지에서 후원박스 4개 다 결제창 뜨도록 함

---
+++ Agenda/WebContent/main/mainpay2.js
@@ -0,0 +1,67 @@
+var pay02 = function(){
+
+	BootPay.request({
+		price: '5000',
+		application_id: "5f17cf9f8f0751002136c6b9",
+		name: '5,000원을 후원하시겠습니까?',
+		pg: '',
+		method: 'card',
+		show_agree_window: 0,
+		items: [
+			{
+				item_name: '5,000원',
+				qty: 1,
+				unique: '123',
+				price: 5000,
+			}
+		],
+		user_info: {
+			username: '사용자 이름',
+			email: '사용자 이메일',
+			addr: '사용자 주소',
+			phone: '010-1234-4567'
+		},
+		order_id: '고유order_id_1234',
+		params: {callback1: '그대로 콜백받을 변수 1', callback2: '그대로 콜백받을 변수 2', customvar1234: '변수명도 마음대로'},
+		account_expire_at: '2018-05-25',
+		extra: {
+		    start_at: '2019-05-10',
+			end_at: '2022-05-10',
+	        vbank_result: 1,
+	        quota: '0,2,3',
+			theme: 'purple',
+			custom_background: '#00a086',
+			custom_font_color: '#ffffff'
+		}
+	}).error(function (data) {
+		console.log(data);
+	}).cancel(function (data) {
+		console.log(data);
+	}).ready(function (data) {
+		console.log(data);
+	}).confirm(function (data) {
+		console.log(data);
+		var enable = true;
+		if (enable) {
+			BootPay.transactionConfirm(data);
+		} else {
+			BootPay.removePaymentWindow();
+		}
+	}).close(function (data) {
+	    console.log(data);
+	}).done(function (data) {
+		console.log(data);
+		$.ajax({
+			url: "dnController",
+			method: "post",
+			data: {"obj" : JSON.stringify(data), command:"donation"},
+			success: function(msg){
+				location.href="./dnController?command=dnlist";
+			},
+			error:function(){
+				alert("통신 실패");
+			}
+		});
+	});
+
+	}
