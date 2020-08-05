cedfecb3d150f935b75557510290066fe32b8832
pionari <hellopionari@gmail.com>
2020-08-05 11:19:02 +0900

    박서윤 : 메인페이지 디자인 (수정중)

--- Agenda/WebContent/main/main.js
+++ Agenda/WebContent/main/main.js
@@ -1,15 +1,4 @@
-function splitScroll() {
-	const controller = new ScrollMagic.Controller();
-
-	new ScrollMagic.Scene({
-		duration : "200%",
-		triggerElement : ".main__about__title",
-		triggerHook : 0,
-	}).setPin(".main__about__title").addTo(controller);
-}
-
-splitScroll();
-	
+// 첫페이지 텍스트
 var text = document.getElementsByClassName('text');
 var shadow = '';
 for (var i = 0; i < 20; i++){
@@ -20,9 +9,39 @@ for (var k = 0; k < 3; k ++){
 	text[k].style.textShadow = shadow;
 }
 
+// 커서
+let cousor = document.getElementById("cursor");
+let cousorArea1 = document.getElementsByClassName("donation");
+let cousorArea2 = document.getElementsByClassName("fourth");
+let cousorArea3 = document.getElementsByClassName("contact");
 
 
+cousorArea2[0].addEventListener("mousemove", function(e) {
+	cursor.style.left = e.pageX + "px";
+	cursor.style.top = e.pageY + "px";
+});
 
+cousorArea3[0].addEventListener("mousemove", function(e) {
+	cursor.style.left = e.pageX + "px";
+	cursor.style.top = e.pageY + "px";
+});
+
+
+// 스크롤
+function splitScroll() {
+	const controller = new ScrollMagic.Controller();
+
+	new ScrollMagic.Scene({
+		duration : "200%",
+		triggerElement : ".fourth-title",
+		triggerHook : 0,
+	}).setPin(".fourth-title").addTo(controller);
+}
+
+splitScroll();
+	
+
+// 도네이션 페이지 효과
 var icon = document.querySelectorAll('.menu-icon-inside')
 
 for(let j = 0; j < icon.length; j++){
